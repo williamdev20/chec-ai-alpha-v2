79c86dbdd63461ae9194696c4381a4693ef499d5
williamdev20 <williamalvs45@gmail.com>
2026-03-15 20:23:49 -0300

    feat: Integrar Google Fact Check Tools API para checar a veracidade do texto extraído do cartaz

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 from paddleocr import PaddleOCR
 from spellchecker import SpellChecker
 import pytesseract
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 ocr = PaddleOCR(use_angle_cls=True, lang="pt", show_log=False)
 spell = SpellChecker(language="pt")
 
-cartaz = "assets/images.jpeg"
+cartaz = "assets/fake-news-cartaz.png"
 
-
+# T1
 def paddleOCR_analyze(img):
     result = ocr.ocr(img)
     extracted_text = " ".join([word[1][0] for line in result for word in line]).lower()
@@ -21,7 +26,7 @@ def paddleOCR_analyze(img):
     return paddleOCR_claim
 
 
-
+# T2
 def tesseract_analyze(img):
     extracted_text = pytesseract.image_to_string(img).lower()
     words = extracted_text.split()
@@ -32,6 +37,8 @@ def tesseract_analyze(img):
     #print("Tesseract OCR Result:", tesseract_text)
     return tesseract_text
     
+
+
 
 
 def check_claim_with_more_correct_words(text):
@@ -46,7 +53,7 @@ def getFinalClaim():
     tesseract_claim = tesseract_analyze(cartaz)
     final_claim = ""
 
-    if check_claim_with_more_correct_words(paddle_claim) > check_claim_with_more_correct_words(tesseract_claim):
+    if check_claim_with_more_correct_words(paddle_claim) < check_claim_with_more_correct_words(tesseract_claim):
         final_claim = paddle_claim
     else:
         final_claim = tesseract_claim
@@ -55,10 +62,46 @@ def getFinalClaim():
 
 
 
+
+def google_fact_checking_claim(query):
+    url = "https://factchecktools.googleapis.com/v1alpha1/claims:search"
+
+    params = {
+        "query": query,
+        "languageCode": "pt",
+        "key": os.getenv("API_KEY")
+    }
+
+    response = requests.get(url, params=params)
+
+    if response.status_code != 200:
+        print("[ERROR]: Houve um erro na requisição: ", response.status_code)
+        return
+    
+    data = response.json()
+
+    if "claims" in data and data["claims"]:
+        """
+        for claim in data["claims"]:
+            reviews = claim["claimReview"]
+            if reviews:
+                print(reviews[0]["textualRating"])
+
+                # Bota um switch case aqui pra ver se é 'Enganoso', 'Verdadeiro', 'Falso', etc
+            else:
+                return False
+                """
+        print(data)
+    else:
+        print("Sem resultados!")
+
+
 if __name__ == "__main__":
     #print(paddleOCR_analyze(cartaz))
     #print(tesseract_analyze(cartaz))
     print(getFinalClaim())
+    google_fact_checking_claim(getFinalClaim())
+    
 
 
 
